30b3ca10baa211bd846b92aae4faf809319760b4
dylanwithgravity <dylanwithgravity@gmail.com>
2018-07-07 21:44:14 -0400

    Adding pods

---
+++ Todoey/Data Model/Data.swift
@@ -0,0 +1,14 @@
+//
+//  Data.swift
+//  Todoey
+//
+//  Created by Dylan Williamson on 7/7/18.
+//  Copyright © 2018 Dylan Williamson. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Data: Object {
+    
+}
