9daffdccddcb4ede353abd786ca2acc4dd57b642
dylanwithgravity <dylanwithgravity@gmail.com>
2018-07-07 20:22:29 -0400

    Install RealmSwift pods

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -61,6 +61,20 @@ class CategoryViewController: UITableViewController {
         
     }
     
+    //MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToItems", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! TodoListViewController
+        
+        if let indexPath = tableView.indexPathForSelectedRow {
+            destinationVC.selectedCategory = categories[indexPath.row]
+        }
+    }
+    
     
     //MARK: - Add New Categories
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
@@ -92,5 +106,5 @@ class CategoryViewController: UITableViewController {
     
     
     
-    //MARK: - TableView Delegate Methods
+    
 }
